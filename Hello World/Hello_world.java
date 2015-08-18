6ca237eee61f1f7e7e88b75995763bb0db698d70
NickNack98 <nickchu98@gmail.com>
2015-08-18 09:19:35 -0700

    Hello World 8/18/15
    This first project was a bit confusing, but that's kind of expected from
    the new software.

---
+++ Hello World/Hello_world.java
@@ -0,0 +1,14 @@
+
+/**
+ * Intro code will do a simple print line.
+ * 
+ * @author (Nicholas Chu) 
+ * @version (8/18/15)
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Nicholas"); //Display the string
+    }
+}
