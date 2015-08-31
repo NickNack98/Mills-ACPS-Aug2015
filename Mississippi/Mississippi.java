560674858e49aa9e933c6dfe580507d8432e2d91
NickNack98 <nickchu98@gmail.com>
2015-08-30 22:58:03 -0700

    Hw

---
+++ Mississippi/Mississippi.java
@@ -0,0 +1,65 @@
+
+/**
+ * Code for the banner version of Mississippi.
+ * 
+ * @author (Nick Chu) 
+ * @version (Version 2 8/30)
+ */
+public class Mississippi
+{
+   public static void main(String[] args){
+       DrawM();
+       DrawI();
+       DrawS();
+       DrawS();
+       DrawI();
+       DrawS();
+       DrawS();
+       DrawI();
+       DrawP();
+       DrawP();
+       DrawI();
+    }
+    public static void DrawM() {
+        System.out.println("M     M");
+        System.out.println("MM   MM");
+        System.out.println("M M M M");
+        System.out.println("M  M  M");
+        System.out.println("M     M");
+        System.out.println("M     M");
+        System.out.println("M     M");
+        System.out.println(" ");
+    }
+    public static void DrawI() {
+        System.out.println("IIIII");
+        System.out.println("  I  ");
+        System.out.println("  I  ");
+        System.out.println("  I  ");
+        System.out.println("  I  ");
+        System.out.println("  I  ");
+        System.out.println("IIIII");
+        System.out.println(" ");
+    }
+    public static void DrawS() {
+        System.out.println(" SSSSS ");
+        System.out.println("S     S");
+        System.out.println("S      ");
+        System.out.println(" SSSSS ");
+        System.out.println("      S");
+        System.out.println("S     S");
+        System.out.println(" SSSSS ");
+        System.out.println(" ");
+    }
+    public static void DrawP() {
+        System.out.println("PPPPPP ");
+        System.out.println("P     P");
+        System.out.println("P     P");
+        System.out.println("PPPPPP ");
+        System.out.println("P      ");
+        System.out.println("P      ");
+        System.out.println("P      ");
+        System.out.println(" ");
+    }
+}
+
+
---
+++ Mississippi/There_Was_an_Old_Lady.java
@@ -0,0 +1,113 @@
+
+/**
+ * Write a description of class There_Was_an_Old_Lady here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class There_Was_an_Old_Lady
+{
+    public static void main(String[] args){
+        Swallow();
+        Fly();
+        Idk();
+        Die();
+        System.out.println("But it's only a fly"); 
+        System.out.println("I think I'll cry");
+        System.out.println("She gulped it out of sky");
+        System.out.println("Oh, my!");
+        Space();
+        Swallow();
+        Spider();
+        System.out.println("\nThat wiggled and jiggled and tickled inside her.");
+        Swallowed();
+        Spider();
+        Catch();
+        Fly();
+        Idk();
+        Die();
+        System.out.println("Gone to the by and by");
+        System.out.println("Sigh");
+        Space();
+        Swallow();
+        Bird();
+        Period();
+        System.out.println("How absurd! She swallowed a bird!");
+        Swallowed();
+        Bird();
+        Catch();
+        Spider();
+        Swallowed2();
+        Spider();
+        Catch();
+        Fly();
+        Idk();
+        Die();
+        System.out.println("She'll leave us high and dry.");
+        Space();
+        Swallow();
+        Cat();
+        Period();
+        System.out.println("Imagine that! She swallowed a cat.");
+        Swallowed();
+        Cat();
+        Catch();
+        Bird();
+        Swallowed2();
+        Bird();
+        Catch();
+        Spider();
+        Swallowed2();
+        Spider();
+        Catch();
+        Fly();
+        Idk();
+        Die();
+        System.out.println("I hope it's a lie.");
+        Space();
+        Swallow();
+        Horse();
+        Period();
+        System.out.println("She died. of course.");
+    }
+    public static void Swallow(){
+        System.out.print("There was an old lady who swallowed a ");
+    }
+    public static void Fly(){
+        System.out.print("fly");
+    }
+    public static void Idk(){
+        System.out.println("\nI don't know why \nShe swallowed the fly");
+    }
+    public static void Die(){
+        System.out.println("Perhaps she'll die.");
+    }
+    public static void Space(){
+        System.out.println(" ");
+    }
+    public static void Spider(){
+        System.out.print("spider");
+    }
+    public static void Swallowed(){
+        System.out.print("She swallowed the ");
+    }
+    public static void Catch(){
+        System.out.print(" to catch the ");
+    }
+    public static void Bird(){
+        System.out.print("bird");
+    }
+    public static void Cat(){
+        System.out.print("cat");
+    }
+    public static void Horse(){
+        System.out.print("horse");
+    }
+    public static void Swallowed2(){
+        System.out.print(".\nShe swallowed the ");
+    }
+    public static void Period(){
+        System.out.println(".");
+    }
+}
+
