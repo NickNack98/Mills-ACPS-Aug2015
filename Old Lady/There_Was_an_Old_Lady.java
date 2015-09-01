b543693d011ed3e8806d7fa07ce2aedbbec08ca0
NickNack98 <nickchu98@gmail.com>
2015-08-31 23:23:45 -0700

    Revision 1.3 and 1.4

---
+++ Old Lady/There_Was_an_Old_Lady.java
@@ -0,0 +1,100 @@
+
+/**
+ * Write a description of class There_Was_an_Old_Lady here.
+ * 
+ * @author (Nicholas Chu) 
+ * @version (Version #2)
+ * @purpose (To print out the there was an old lady song in Java)
+ */
+public class There_Was_an_Old_Lady
+{
+    public static void main(String[] args){
+        thereWasAnOldLady();
+        animalFly();
+        iDontKnowWhy();
+        System.out.println("But it's only a fly"); 
+        System.out.println("I think I'll cry");
+        System.out.println("She gulped it out of sky");
+        System.out.println("Oh, my!\n");
+        thereWasAnOldLady();
+        animalSpider();
+        System.out.println("\nThat wiggled and jiggled and tickled inside her.");
+        sheSwallowed();
+        animalSpider();
+        toCatch();
+        animalFly();
+        iDontKnowWhy();
+        System.out.println("Gone to the by and by");
+        System.out.println("Sigh\n");
+        thereWasAnOldLady();
+        animalBird();
+        conventionPeriod();
+        System.out.println("How absurd! She swallowed a bird!");
+        sheSwallowed();
+        animalBird();
+        toCatch();
+        animalSpider();
+        sheSwallowedNext();
+        animalSpider();
+        toCatch();
+        animalFly();
+        iDontKnowWhy();
+        System.out.println("She'll leave us high and dry.\n");
+        thereWasAnOldLady();
+        animalCat();
+        conventionPeriod();
+        System.out.println("Imagine that! She swallowed a cat.");
+        sheSwallowed();
+        animalCat();
+        toCatch();
+        animalBird();
+        sheSwallowedNext();
+        animalBird();
+        toCatch();
+        animalSpider();
+        sheSwallowedNext();
+        animalSpider();
+        toCatch();
+        animalFly();
+        iDontKnowWhy();
+        System.out.println("I hope it's a lie.\n");
+        thereWasAnOldLady();
+        animalHorse();
+        conventionPeriod();
+        System.out.println("She died. of course.");
+    }
+    public static void thereWasAnOldLady(){
+        System.out.print("There was an old lady who swallowed a ");
+    }
+    public static void animalFly(){
+        System.out.print("fly");
+    }
+    public static void iDontKnowWhy(){
+        System.out.println("\nI don't know why \nShe swallowed the fly\nPerhaps she'll die.");
+    }
+    public static void animalSpider(){
+        System.out.print("spider");
+    }
+    public static void sheSwallowed(){
+        System.out.print("She swallowed the ");
+    }
+    public static void toCatch(){
+        System.out.print(" to catch the ");
+    }
+    public static void animalBird(){
+        System.out.print("bird");
+    }
+    public static void animalCat(){
+        System.out.print("cat");
+    }
+    public static void animalHorse(){
+        System.out.print("horse");
+    }
+    public static void sheSwallowedNext(){
+        System.out.print(".\nShe swallowed the ");
+    }
+    public static void conventionPeriod(){
+        System.out.println(".");
+    }
+}
+
